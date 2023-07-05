5e729eef957189b0b7814635eac6cbdc6c3bef79
sixmono <2043805383@qq.com>
2023-07-05 18:10:49 +0800

    first push

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,42 +1,92 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <!-- 这段代码是界面显示 -->
+    <div v-for="(attachment, index) in attachmentList" :key="index">
+      <el-link
+        :key="attachment.path"
+        :href="attachment.path"
+        style="display: inline-block"
+        type="success"
+        :underline="false"
+      >
+        {{ attachment.name }}
+      </el-link>
+      <el-button
+        type="text"
+        style="display: inline-block; margin-left: 30px"
+        @click="previewFile(attachment)"
+      >
+        预览</el-button
+      >
+    </div>
+
+    <!-- 点击上面的预览按钮会弹出文件预览框 -->
+    <el-dialog
+      :close-on-click-modal="true"
+      title="文件预览"
+      type="primary"
+      v-modal="previewDialog"
+      width="80%"
+      left
+    >
+      <iframe :src="attachmentSrc" frameborder="0" width="100%" height="800"></iframe>
+    </el-dialog>
+    <el-button type="primary" @click="close">关s闭</el-button>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
   props: {
-    msg: String
-  }
-}
+    msg: String,
+  },
+
+  data() {
+    return {
+      previewDialog: false,
+      attachmentSrc: "",
+      attachmentList: [
+        {
+          name: "example1.docx",
+          path:
+            "http://allen2023-06-09.oss-cn-beijing.aliyuncs.com/world.doc?Expires=1688576856&OSSAccessKeyId=TMP.3KdCDaVhtJEFwiwSLsGY64JxdzpJNtvX7eJWFoHkSrE7HHq69GNzzR6DEM6LZuoqFZ6QSFQEs5vR8xXdrx9hkrWdk6GkJD&Signature=erSCKBh%2BLsYu%2BH6mu5T1sDZVhGQ%3D",
+        },
+        {
+          name: "example2.pdf",
+          path:
+            "http://storage.xuetangx.com/public_assets/xuetangx/PDF/PlayerAPI_v1.0.6.pdf",
+        },
+        {
+          name: "example3.txt",
+          path: "https://vfdgfdsgdfsgfg/attachment/example3.txt",
+        },
+        {
+          name: "example4.xlsx",
+          path: "https://vfdgfdsgdfsgfg/attachment/example4.xlsx",
+        },
+      ],
+    };
+  },
+
+  methods: {
+    // 预览文件
+    previewFile(attachment) {
+      // 根据文件格式显示预览内容
+      const fileExtension = attachment.path.split(".").pop().toLowerCase();
+      if (fileExtension === "xlsx" || fileExtension === "docx") {
+        this.attachmentSrc =
+          "https://view.officeapps.live.com/op/view.aspx?src=" + attachment.path;
+      } else {
+        this.attachmentSrc = attachment.path;
+      }
+      this.previewDialog = true;
+    },
+    close() {
+      this.previewDialog = false;
+    },
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
